6f1a3dff71e0fde5e84e7e70aa489ed639069242
AviDaPro <c4unc4un@gmail.com>
2023-04-24 21:09:25 -0700

    Added turtle to main
    Signed-off-by: AviDaPro <c4unc4un@gmail.com>

--- Java/IntroToJava.java
+++ Java/IntroToJava.java
@@ -8,12 +8,14 @@ public class IntroToJava {
     	System.out.println("Hello!");
         //MakeTurtle.make();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Would you like to use calculator (0) or play Rock/paper/scissors (1)? ");
+        System.out.println("Would you like to use calculator (0) or play Rock/paper/scissors (1) or make turtle (2)? ");
         int choice = sc.nextInt();
         if (choice == 0) {
             Calculator.calculate();
-        } else {
+        } else if (choice == 1) {
             RockPaper.play();
+        } else {
+            MakeTurtle.make();
         }
 
         sc.close();
